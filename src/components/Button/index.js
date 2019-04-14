0c33cd526e65f1f81b09f204b48c98b9045a2f49
Naxulanth <deniz@deniz.gg>
2019-04-15 02:55:13 +0300

    fontsize

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 
 const ButtonComponent = styled(Button)`
 margin-right: 10px;
-font-size: 14px !important;
+font-size: 12px !important;
+padding: 3px !important;
 `
 
 export default ButtonComponent;
