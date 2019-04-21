16160654a8337e323fc2b8f09ecab3afc0afe0bc
Naxulanth <deniz@deniz.gg>
2019-04-22 00:24:06 +0300

    filled in selectors

--- src/constants/attributes.js
+++ src/constants/attributes.js
@@ -1,7 +1,20 @@
 import Option from "containers/Option";
 import OptionColor from "containers/OptionColor";
 import OptionSelect from "containers/OptionSelect";
-import { borderStyle, outlineStyle, clearStyle } from "constants/options";
+import {
+  borderStyle,
+  outlineStyle,
+  clearStyle,
+  cursorStyle,
+  displayStyle,
+  floatStyle,
+  positionStyle,
+  overflowStyle,
+  verticalAlignStyle,
+  textAlignStyle,
+  textDecorationStyle,
+  textTransformStyle
+} from "constants/options";
 
 export const attributes = [
   { label: "border-left-width", value: "border-left-width", type: Option },
@@ -57,17 +70,36 @@ export const attributes = [
     type: Option
   },
   { label: "clear", value: "clear", type: OptionSelect, option: clearStyle },
-  { label: "cursor", value: "cursor", type: OptionSelect }, //
-  { label: "display", value: "display", type: OptionSelect }, //
-  { label: "float", value: "float", type: OptionSelect }, //
-  { label: "position", value: "position", type: OptionSelect }, //
+  { label: "cursor", value: "cursor", type: OptionSelect, option: cursorStyle },
+  {
+    label: "display",
+    value: "display",
+    type: OptionSelect,
+    option: displayStyle
+  },
+  { label: "float", value: "float", type: OptionSelect, option: floatStyle },
+  {
+    label: "position",
+    value: "position",
+    type: OptionSelect,
+    option: positionStyle
+  },
   { label: "top", value: "top", type: Option },
   { label: "left", value: "left", type: Option },
   { label: "bottom", value: "bottom", type: Option },
   { label: "right", value: "right", type: Option },
-  { label: "overflow", value: "overflow", type: OptionSelect }, //
-  { label: "position", value: "position", type: OptionSelect }, //
-  { label: "vertical-align", value: "vertical-align", type: OptionSelect }, //
+  {
+    label: "overflow",
+    value: "overflow",
+    type: OptionSelect,
+    option: overflowStyle
+  },
+  {
+    label: "vertical-align",
+    value: "vertical-align",
+    type: OptionSelect,
+    option: verticalAlignStyle
+  },
   {
     label: "z-index",
     value: "z-index",
@@ -83,15 +115,20 @@ export const attributes = [
     type: Option,
     pxOption: true
   },
-  { label: "text-align", value: "text-align", type: OptionSelect }, //
-  { label: "text-decoration", value: "text-decoration", type: OptionSelect }, //
+  {
+    label: "text-align",
+    value: "text-align",
+    type: OptionSelect,
+    option: textAlignStyle
+  },
+  { label: "text-decoration", value: "text-decoration", type: OptionSelect, option: textDecorationStyle },
   { label: "text-indent", value: "text-indent", type: Option },
-  { label: "text-capitalize", value: "text-capitalize", type: OptionSelect }, //
+  { label: "text-transform", value: "text-transform", type: OptionSelect, option: textTransformStyle },
   {
     label: "word-spacing",
     value: "word-spacing",
     type: Option,
-    pxOption: true,
+    pxOption: true
   }
 ].sort((a, b) => {
   if (a.label < b.label) return -1;
--- src/constants/options.js
+++ src/constants/options.js
@@ -30,3 +30,78 @@ export const clearStyle = [
   { label: "right", value: "right" },
   { label: "none", value: "none" }
 ];
+
+export const cursorStyle = [
+  { label: "url", value: "url" },
+  { label: "auto", value: "auto" },
+  { label: "crosshair", value: "crosshair" },
+  { label: "default", value: "default" },
+  { label: "pointer", value: "pointer" },
+  { label: "move", value: "move" },
+  { label: "text", value: "text" },
+  { label: "wait", value: "wait" },
+  { label: "help", value: "help" }
+];
+
+export const displayStyle = [
+  { label: "none", value: "none" },
+  { label: "inline", value: "inline" },
+  { label: "block", value: "block" },
+  { label: "list-item", value: "list-item" },
+  { label: "table-cell", value: "table-cell" },
+  { label: "table-row", value: "table-row" },
+  { label: "inline-block", value: "inline-block" }
+];
+
+export const floatStyle = [
+  { label: "left", value: "left" },
+  { label: "right", value: "right" },
+  { label: "none", value: "none" }
+];
+
+export const positionStyle = [
+  { label: "static", value: "static" },
+  { label: "relative", value: "relative" },
+  { label: "absolute", value: "absolute" },
+  { label: "fixed", value: "fixed" }
+];
+
+export const overflowStyle = [
+  { label: "visible", value: "visible" },
+  { label: "hidden", value: "hidden" },
+  { label: "scroll", value: "scroll" },
+  { label: "auto", value: "auto" }
+];
+
+export const verticalAlignStyle = [
+  { label: "top", value: "top" },
+  { label: "bottom", value: "bottom" },
+  { label: "middle", value: "middle" },
+  { label: "text-bottom", value: "text-bottom" },
+  { label: "text-top", value: "text-top" },
+  { label: "super", value: "super" },
+  { label: "sub", value: "sub" },
+  { label: "baseline", value: "baseline" }
+];
+
+export const textAlignStyle = [
+  { label: "left", value: "left" },
+  { label: "right", value: "right" },
+  { label: "center", value: "center" },
+  { label: "justify", value: "justify" }
+];
+
+export const textDecorationStyle = [
+  { label: "none", value: "none" },
+  { label: "underline", value: "underline" },
+  { label: "overline", value: "overline" },
+  { label: "line-through", value: "line-through" },
+  { label: "blink", value: "blink" }
+];
+
+export const textTransformStyle = [
+  { label: "none", value: "none" },
+  { label: "capitalize", value: "capitalize" },
+  { label: "uppercase", value: "uppercase" },
+  { label: "lowercase", value: "lowercase" }
+];
